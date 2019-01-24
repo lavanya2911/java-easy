c2df34ab40d3d8cf8b2d43278174bb1b87965012
lavanya2911 <46344347+lavanya2911@users.noreply.github.com>
2019-01-24 16:17:03 +0530

    Add files via upload

---
+++ what is median.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+class Main
+{
+	public static void main(String[] args)
+	{
+		Scanner in=new Scanner(System.in);
+		ArrayList<Integer> a=new ArrayList<Integer>();
+		while(in.hasNextInt())
+		{
+			
+			int s=in.nextInt();
+			a.add(s);
+			
+			for(int i=0;i<1;i++)
+			{
+				Collections.sort(a);
+				if(a.size()%2==0)
+				{
+					System.out.println((a.get(a.size()/2-1)+a.get(a.size()/2))/2);
+					if(a.size()==0)
+						System.out.println(a.get(0));
+				}
+				else 
+					System.out.println(a.get(a.size()/2));
+			}
+		}
+	}
+}
+
